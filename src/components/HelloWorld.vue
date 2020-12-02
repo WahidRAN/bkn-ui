68dc2cc46647446913b41d3ae232eaf2bb7b8883
Wahid Rizqi Akbar Nurjaman <wahid.rizqi@midasteknologi.com>
2020-12-02 09:21:20 +0700

    Halaman Beranda Awal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,84 +1,309 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+  <v-app>
+    <v-app-bar app dense color="primary" height="70">
+      <v-avatar class="mr-3" color="primary" size="70">
+        <v-img contain max-height="100%" src="../assets/logo.png"></v-img>
+      </v-avatar>
+      <v-spacer></v-spacer>
+      <v-tabs right color="white">
+        <v-tab>Beranda</v-tab>
+        <v-tab>Profil</v-tab>
+        <v-tab>Wilayah Kerja</v-tab>
+        <v-tab>Berita</v-tab>
+        <v-tab>Regulasi</v-tab>
+        <v-tab>Galeri</v-tab>
+        <v-tab>Pertanyaan Umum</v-tab>
+      </v-tabs>
+    </v-app-bar>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+    <v-content>
+      <section id="hero">
+        <v-row no-gutters>
+          <v-img src="../assets/hero.jpg">
+            <v-theme-provider dark>
+              <v-container fill-height>
+                <v-row
+                  align="center"
+                  class="white--text mx-auto"
+                  justify="center"
+                >
+                  <v-col class="primary--text text-center" cols="12" tag="h1">
+                    <span
+                      :class="[
+                        $vuetify.breakpoint.smAndDown
+                          ? 'display-1'
+                          : 'display-2'
+                      ]"
+                      class="font-weight-light"
+                    >
+                      Kantor Regional V
+                    </span>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
+                    <br />
 
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+                    <span
+                      :class="[
+                        $vuetify.breakpoint.smAndDown
+                          ? 'display-3'
+                          : 'display-4'
+                      ]"
+                      class="font-weight-black"
+                    >
+                      BKN Jakarta
+                    </span>
+                  </v-col>
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
+                  <v-btn
+                    class="align-self-end"
+                    color="black"
+                    fab
+                    outlined
+                    @click="$vuetify.goTo('#about-me')"
+                  >
+                    <v-icon>mdi-chevron-double-down</v-icon>
+                  </v-btn>
+                </v-row>
+              </v-container>
+            </v-theme-provider>
+          </v-img>
         </v-row>
-      </v-col>
+      </section>
 
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
+      <section id="about-me">
+        <div class="py-12"></div>
 
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">VISI</h2>
+
+          <v-responsive class="mx-auto mb-8" width="56">
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-responsive
+            class="mx-auto title font-weight-light mb-8"
+            max-width="720"
           >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
+            “Menjadi Pembina dan Penyelenggara Manajemen Kepegawaian yang
+            Profesional dan Bermartabat Tahun 2025”
+          </v-responsive>
 
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
+          <!-- <v-avatar class="elevation-12 mb-12" size="128">
+            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+          </v-avatar>
 
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
+          <div></div>
+
+          <v-btn color="grey" href="https://vuetifyjs.com" outlined large>
+            <span class="grey--text text--darken-1 font-weight-bold">
+              Vuetify Documentation
+            </span>
+          </v-btn> -->
+        </v-container>
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">MISI</h2>
+
+          <v-responsive class="mx-auto mb-8" width="56">
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-responsive
+            class="mx-auto title font-weight-light mb-8"
+            max-width="720"
           >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+            <v-row>
+              <v-col class="mx-2"
+                >Mengembangkan Sistem Manajemen Kepegawaian Negara</v-col
+              >
+              <v-col class="mx-2"
+                >Mengembangkan Sistem Pelayanan Kepegawaian</v-col
+              >
+              <v-col class="mx-2">Mengembangkan Manajemen Internal BKN</v-col>
+            </v-row>
+          </v-responsive>
+
+          <!-- <v-avatar class="elevation-12 mb-12" size="128">
+            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+          </v-avatar>
+
+          <div></div>
+
+          <v-btn color="grey" href="https://vuetifyjs.com" outlined large>
+            <span class="grey--text text--darken-1 font-weight-bold">
+              Vuetify Documentation
+            </span>
+          </v-btn> -->
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+
+      <section id="blog">
+        <div class="py-12"></div>
+
+        <v-container>
+          <h2
+            class="display-2 font-weight-bold mb-3 text-uppercase text-center"
+          >
+            Berita Terkini
+          </h2>
+
+          <v-responsive class="mx-auto mb-12" width="56">
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-row>
+            <v-col
+              v-for="({ src, text, title }, i) in articles"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-img
+                :src="src"
+                class="mb-4"
+                height="275"
+                max-width="100%"
+              ></v-img>
+
+              <h3
+                class="font-weight-black mb-4 text-uppercase"
+                v-text="title"
+              ></h3>
+
+              <div class="title font-weight-light mb-5" v-text="text"></div>
+
+              <v-row>
+                <v-spacer></v-spacer>
+                <v-btn class="ml-n4 font-weight-black" color="secondary">
+                  Lanjutkan membaca
+                </v-btn>
+                <v-spacer></v-spacer>
+              </v-row>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-spacer></v-spacer>
+            <v-btn class="mt-16" color="primary">Berita Lainnya</v-btn>
+            <v-spacer></v-spacer>
+          </v-row>
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+
+      <section id="features" class="grey lighten-3">
+        <div class="py-12"></div>
+
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">LAYANAN</h2>
+
+          <v-responsive class="mx-auto mb-12" width="56">
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-row>
+            <v-col
+              v-for="({ icon, title, text }, i) in features"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-card class="py-12 px-4" color="grey lighten-5" flat>
+                <v-theme-provider dark>
+                  <div>
+                    <v-avatar color="primary" size="88">
+                      <v-icon large v-text="icon"></v-icon>
+                    </v-avatar>
+                  </div>
+                </v-theme-provider>
+
+                <v-card-title
+                  class="justify-center font-weight-black text-uppercase"
+                  v-text="title"
+                ></v-card-title>
+
+                <v-card-text class="subtitle-1" v-text="text"> </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+
+      <section id="stats">
+        <v-parallax
+          :height="$vuetify.breakpoint.smAndDown ? 700 : 500"
+          src="https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+        >
+          <v-container fill-height>
+            <v-row class="mx-auto">
+              <v-col
+                v-for="[value, title] of stats"
+                :key="title"
+                cols="12"
+                md="3"
+              >
+                <div class="text-center">
+                  <div
+                    class="display-3 font-weight-black mb-4"
+                    v-text="value"
+                  ></div>
+
+                  <div
+                    class="title font-weight-regular text-uppercase"
+                    v-text="title"
+                  ></div>
+                </div>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-parallax>
+      </section>
+
+      <v-sheet id="contact" color="#333333" dark tag="section" tile>
+        <div class="py-12"></div>
+
+        <v-container>
+          <h2
+            class="display-2 font-weight-bold mb-3 text-uppercase text-center"
+          >
+            Kontak Kami
+          </h2>
+
+          <v-responsive class="mx-auto mb-12" width="56">
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-row>Kantor Regional V Badan Kepegawaian Negara Jakarta</v-row>
+          <v-row>Jalan Raya Ciracas No. 36, Jakarta Timur</v-row>
+          <v-row>Telp. 021-87721084/89</v-row>
+          <v-row>Fax. 021-87721085</v-row>
+          <v-row>e-mail : kanreg5.jakarta@bkn.go.id</v-row>
+        </v-container>
+
+        <div class="py-12"></div>
+      </v-sheet>
+    </v-content>
+
+    <v-footer class="justify-center" color="#292929" height="100">
+      <div
+        class="title font-weight-light grey--text text--lighten-1 text-center"
+      >
+        &copy; {{ new Date().getFullYear() }} Kanreg V Jakarta BKN
+      </div>
+    </v-footer>
+  </v-app>
 </template>
 
 <script lang="ts">
@@ -87,57 +312,64 @@ import Vue from "vue";
 export default Vue.extend({
   name: "HelloWorld",
 
-  data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
+  data() {
+    return {
+      articles: [
+        {
+          src:
+            "https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+          title: "Berita A",
+          text:
+            "Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum."
+        },
+        {
+          src:
+            "https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+          title: "Berita B",
+          text:
+            "Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh."
+        },
+        {
+          src:
+            "https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80",
+          title: "Berita C",
+          text:
+            "Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget."
+        }
+      ],
+      features: [
+        {
+          icon: "mdi-account-group-outline",
+          title: "LAYANAN A",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam"
+        },
+        {
+          icon: "mdi-update",
+          title: "LAYANAN B",
+          text:
+            "Sed ut elementum justo. Suspendisse non justo enim. Vestibulum cursus mauris dui, a luctus ex blandit. Lorem ipsum dolor sit amet consectetur adipisicing elit. qui ipsum eveniet facilis obcaecati corrupti consectetur adipisicing elit."
+        },
+        {
+          icon: "mdi-shield-outline",
+          title: "LAYANAN C",
+          text:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam"
+        }
+      ],
+      stats: [
+        ["316", "Pengunjung Hari ini"],
+        ["17361", "Pengunjung Bulan ini"],
+        ["253397", "Pengunjung Tahun ini"],
+        ["253397", "Total Pengunjung"]
+      ]
+    };
+  }
 });
 </script>
+
+<style scoped>
+.hero {
+  text-shadow: 2px 2px #272424;
+}
+</style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,13 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#96858F",
+        secondary: "#6D7993"
+      }
+    }
+  }
+});
